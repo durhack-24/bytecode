a0ec9af4b28d0072695c1f6b52637bb94094e069
MaxAFriedrich <58374387+MaxAFriedrich@users.noreply.github.com>
2024-11-02 23:42:59 +0000

    slur support

--- bytecode/score/muse.py
+++ bytecode/score/muse.py
@@ -1,13 +1,21 @@
 from dataclasses import dataclass
+from enum import EnumType
 from xml.etree import ElementTree as ET
 
 from bytecode.score.utils import Duration
+
+
+class SlurState(EnumType):
+    NO_SLUR = 0
+    SLUR_START = 1
+    SLUR_END = 2
 
 
 @dataclass
 class Note:
     pitch: int
     duration: int
+    slur_state: int = SlurState.NO_SLUR
 
 
 class Voice:
@@ -30,7 +38,6 @@ class Voice:
         self.notes: list[Note] = []
         for child in voice_elm:
             if child.tag == "TimeSig":
-                print("TimeSig")
                 for grand_child in child:
                     if grand_child.tag == "sigN":
                         self.time_sig_n = int(grand_child.text)
@@ -38,14 +45,24 @@ class Voice:
                         self.time_sig_d = int(grand_child.text)
             elif child.tag == "Chord":
                 ticks = 0
+                slur_state = SlurState.NO_SLUR
                 for grand_child in child:
                     if grand_child.tag == "durationType":
                         ticks = Duration.parse_duration(grand_child.text)
+                    if grand_child.tag == "Spanner":
+                        if grand_child.attrib.get("type", "") == "Slur":
+                            for great_grand_child in grand_child:
+                                if great_grand_child.tag == "next":
+                                    slur_state = SlurState.SLUR_START
+                                if great_grand_child.tag == "prev":
+                                    slur_state = SlurState.SLUR_END
                     if grand_child.tag == "Note":
                         for great_grand_child in grand_child:
                             if great_grand_child.tag == "pitch":
                                 self.notes.append(
-                                    Note(int(great_grand_child.text), ticks))
+                                    Note(int(great_grand_child.text), ticks,
+                                         slur_state=slur_state))
+
             elif child.tag == "Rest":
                 ticks = self.time_sig
                 for grand_child in child:

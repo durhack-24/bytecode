6a724faf6fd12767ce1a387b130afb6ed3b9a82d
MaxAFriedrich <58374387+MaxAFriedrich@users.noreply.github.com>
2024-11-02 21:24:29 +0000

    start adding wrappers
    include package

--- bytecode/__main__.py
+++ bytecode/__main__.py
@@ -1,7 +1,17 @@
-
+from pathlib import Path
 
 if __name__ == "__main__":
     import sys
     from . import main
+    input_file = Path(sys.argv[1])
+    output_file = Path(sys.argv[2])
 
-    sys.exit(main.main())
+    if not input_file.exists() or not input_file.is_file():
+        print("Input file does not exist or is not a file")
+        sys.exit(1)
+
+    if input_file.suffix != ".xml":
+        print("Input file must be a xml file")
+        sys.exit(1)
+
+    sys.exit(main.main(input_file, output_file))
--- bytecode/main.py
+++ bytecode/main.py
@@ -3,5 +3,7 @@ from pathlib import Path
 from bytecode.score import load_score
 
 
-def main():
-    print(load_score(Path('./example_data/score.xml')))
+def main(input_file: Path, output_file: Path):
+    object_score = load_score(input_file)
+    print(object_score)
+    output_file.write_text(object_score.serialize())
